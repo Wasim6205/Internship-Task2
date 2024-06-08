bfb6e943454acaa672245638cdbf6c640126990b
Wasim <wasimbhai6205@gmail.com>
2024-06-08 15:41:40 +0530

    animation added

--- Script/script.js
+++ Script/script.js
@@ -15,10 +15,10 @@ $(".owl-carousel").owlCarousel({
     },
   });
 
-
-
   document.addEventListener("DOMContentLoaded", function() {
     // const dott = document.getElementsByClassName("dotsst")
+    const image1 = document.querySelector('.animImage1');
+    const image2 = document.querySelector('.animImage2');
     const dots = document.querySelectorAll(".dots-nav");
     // const mainImage = document.getElementById("main-image");
     const leftColor =document.getElementById("left")
@@ -26,19 +26,23 @@ $(".owl-carousel").owlCarousel({
 
     dots.forEach(dot => {
         dot.addEventListener("click", function() {
-            // const newImage = this.getAttribute("data-image");
+            const newImage1 = this.getAttribute("data-image1");
+            const newImage2 = this.getAttribute("data-image2");
             const newColor1 = this.getAttribute("data-color-1");
             const newColor2 = this.getAttribute("data-color-2");
 
             // mainImage.style.opacity = 0;
 
             setTimeout(() => {
-                // mainImage.src = newImage;
+                image1.src = newImage1;
+                image2.src = newImage2;
                 leftColor.style.backgroundColor = newColor1;
                 rightColor.style.backgroundColor = newColor2;
+                animate()
                 // mainImage.style.opacity = 1;
                 // dott.style.opacity = 0;
             }, 500);
         });
     });
 });
+
